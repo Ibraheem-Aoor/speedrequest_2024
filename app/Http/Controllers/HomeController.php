799d4b2fa0badb18f2996929214f7cef5eff5c57
Ibraheem <ibraheem.alaoor@hotmail.com>
2024-07-10 18:01:42 +0300

    up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,6 +62,7 @@ class HomeController extends BaseSiteContoller
         ]);
         session()->put('current_order_id', $current_order->id);
         session()->put('has_visited_cpa_page', 'YES');
+        session()->put('last_visit_order_' . $current_order->id, time());
         $service = $this->service_service->find(($service_id));
         return redirect($service->offer_url);
     }
@@ -73,25 +74,46 @@ class HomeController extends BaseSiteContoller
      * @param \Illuminate\Http\Request $request
      * @return void
      */
+    /**
+     * This is the callback the user is redirected to from the CPA dashboard.
+     * If the incoming IP is the same as the user IP, this means the user tried to bypass the challenge.
+     * Otherwise, the user has completed the challenge.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function taskCompleted(Request $request)
     {
         if (
-            session()->has('has_visited_cpa_page') && session()->get('has_visited_cpa_page') == 'YES' &&
-            $request->ip() != session()->get('user_ip') && session()->has('current_order_id')
+            session()->has('has_visited_cpa_page') && session()->get('has_visited_cpa_page') == 'YES' && session()->has('current_order_id')
         ) {
-            $data['order'] = Order::query()->where('id', session()->get('current_order_id'))->first();
-            if(!isset($data['order']))
-            {
+            $data['order'] = Order::query()->find(session()->get('current_order_id'));
+
+            if (!isset($data['order'])) {
                 return abort(404);
             }
-            return view('site.task_completed', $data);
+
+            // Calculate the time elapsed since the last visit
+            $lastVisitTime = session()->get('last_visit_order_' . $data['order']->id);
+            $elapsedTime = time() - $lastVisitTime;
+
+            // Minimum time to stay (90 seconds)
+            $minimumStaySeconds = 90;
+
+            // Check if the minimum stay time has been reached
+            if ($elapsedTime >= $minimumStaySeconds) {
+                return view('site.task_completed', $data);
+            } else {
+                return abort(404);
+            }
         }
+
         abort(404);
     }
 
-    public function confirmOrder(ConfirmOrderRequest $request ,$order_id , OrderService $order_service)
+    public function confirmOrder(ConfirmOrderRequest $request, $order_id, OrderService $order_service)
     {
-        return $order_service->update(decrypt($order_id) , $request);
+        return $order_service->update(decrypt($order_id), $request);
     }
 
     public function contact(): View
